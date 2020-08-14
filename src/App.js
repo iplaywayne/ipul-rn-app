a45664ca5816d54fafd08379224f9c1ee4f1b229
iPlay uListen <iplayulisten@iPlays-MacBook-Pro.local>
2020-08-14 00:31:21 -0400

    yarn commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  View, Text, TextInput, Button, TouchableOpacity, Image, ActivityIndicator
+  View, Text, TextInput, TouchableOpacity, Image, ActivityIndicator, Alert
 } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Spinner from 'react-native-spinkit'
+import PropTypes from 'prop-types'
+import { Input, Block } from 'galio-framework';
 
+
+import Button from 'react-native-button'
+import { firebase } from './utils/firebase'
 import SplashScreen from './screens/SplashScreen/SplashScreen'
 import { SignIn as SignInScreen } from './screens/SignIn'
 import { SignUp as SignUpScreen } from './screens/SignUp'
@@ -16,15 +21,71 @@ import { Store, useStore } from './utils/store'
 import { Center } from './components/Center'
 import AuthProvider, { useAuth } from './contexts/AuthContext'
 
+
 const AuthStack = createStackNavigator();
 
+App.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string
+  })
+}
 
-export default function () {
+function App() {
   const [authState, authDispatch] = useAuth()
   const [storeState, storeDispatch] = useStore()
   const { user, isLoading, userToken } = authState
-  
+  const [newUserName, setNewUserName] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+
   if (isLoading) return <Center><Spinner type='Wave' /></Center>
+
+  const handleCreateName = () => {
+    const usrRef = firebase.database().ref(`users/`)
+    usrRef.on('value', snap => {
+      let list = []
+      snap.forEach(child => {
+        if (child.val().name === newUserName) {
+          list.push(child.val())
+          setTimeout(() => {
+            Alert.alert('This Username exists. Try again')
+          }, 1000)
+        }
+      })
+      if (!list.length) {
+        setTimeout(() => {
+          Alert.alert('This username is available')
+        }, 1000)
+      }
+    })
+  }
+
+  if (user && !('name' in user)) return <Center>
+    <Text style={{ fontWeight: '700', fontSize: 25, marginBottom: 10 }}>Create Username</Text>
+    <View style={{ width: 300, alignItems: 'center' }}>
+      <Input
+        placeholder="Enter your unique Username"
+        right
+        icon="check"
+        family="antdesign"
+        iconSize={14}
+        iconColor="green"
+        placeholderTextColor={'#121212'}
+        onChange={e => setNewUserName(e.nativeEvent.text)}
+      />
+      <Button
+        style={{ fontSize: 15, color: 'white', width: '100%' }}
+        containerStyle={{
+          padding: 10, marginTop: 25, height: 40, width: 150,
+          overflow: 'hidden', borderRadius: 5, backgroundColor: '#121212'
+        }}
+        disabledContainerStyle={{ backgroundColor: '#ddd' }}
+        onPress={handleCreateName}
+      >
+        {loading ? <ActivityIndicator /> : 'Continue'}
+      </Button>
+    </View>
+  </Center>
+
 
   return (
     <NavigationContainer>
@@ -44,7 +105,7 @@ export default function () {
             animationTypeForReplace: userToken ? 'pop' : 'push'
           }} />
         </AuthStack.Navigator>
-        
+
         :
 
         <TabNavigator />
@@ -53,3 +114,5 @@ export default function () {
     </NavigationContainer>
   )
 }
+
+export default App
--- src/components/Prebuilt/Button.js
+++ src/components/Prebuilt/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from 'react-native-button'
+import { ActivityIndicator } from 'react-native'
 
 
 export default function (props) {
-  const { title, style, disabled, children, onPress, backgroundColor, color } = props
+  const { title, style, loading, disabled, children, marginTop, onPress, backgroundColor, color } = props
 
   return (
     <Button
@@ -11,13 +12,14 @@ export default function (props) {
       styleDisabled={{ color: 'white' }}
       disabled={disabled}
       containerStyle={{
-        padding: 7, margin: 1, height: 30, width: 130, overflow: 'hidden', borderRadius: 5,
+        paddingTop: 8, margin: marginTop || 1, height: 32, width: 130, overflow: 'hidden', borderRadius: 5,
         backgroundColor: color ? color : backgroundColor || '#121212'
       }}
       disabledContainerStyle={{ backgroundColor: '#ddd' }}
       onPress={onPress}
     >
-      {title || children}
+
+      {loading ? <ActivityIndicator /> : title || children}
     </Button>
   )
 }
